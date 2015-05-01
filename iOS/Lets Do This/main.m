a0e327de2f45c99e2579a771f1955c66d6c26769
Ryan Grimm <grimm@xqdev.com>
2015-05-01 12:10:09 -0700

    Including a few dangling files.

---
+++ iOS/Lets Do This/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lets Do This
+//
+//  Created by Ryan Grimm on 4/2/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
