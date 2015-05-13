48eaef9c0ae54561677a298f20c54e9f8a4bdf45
Aaron Schachter <aaronschachter@gmail.com>
2015-05-13 16:07:17 -0700

    Adds skeleton Settings menu

--- Lets Do This/Controllers/LDTSettingsViewController.m
+++ Lets Do This/Controllers/LDTSettingsViewController.m
@@ -10,4 +10,81 @@
 
 @implementation LDTSettingsViewController
 
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Settings";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 4;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return @"Account";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"settingsCell" forIndexPath:indexPath];
+    NSString *label;
+    switch (indexPath.row) {
+        case 0:
+            label = @"Change Photo";
+            break;
+        case 1:
+            label = @"Change Email";
+            break;
+        case 2:
+            label = @"Change Password";
+            break;
+        default:
+            label = @"Logout";
+            break;
+    }
+    cell.textLabel.text = label;
+    [cell setUserInteractionEnabled:YES];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch(indexPath.row) {
+        case 3:
+            [self logoutActionSheet];
+        default:
+            break;
+    }
+}
+
+- (void) logoutActionSheet {
+    UIAlertController *view = [UIAlertController alertControllerWithTitle:@"Are you sure you want to log out?"
+                                     message:@"Don't worry, it's cool if you do."
+                                     preferredStyle:UIAlertControllerStyleActionSheet];
+
+        UIAlertAction *confirm = [UIAlertAction
+                             actionWithTitle:@"Log Out"
+                             style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction * action)
+                             {
+                                 [view dismissViewControllerAnimated:YES completion:nil];
+                             }];
+        UIAlertAction *cancel = [UIAlertAction
+                                 actionWithTitle:@"Cancel"
+                                 style:UIAlertActionStyleDefault
+                                 handler:^(UIAlertAction * action)
+                                 {
+                                     [view dismissViewControllerAnimated:YES completion:nil];
+
+                                 }];
+        
+        
+        [view addAction:confirm];
+        [view addAction:cancel];
+        [self presentViewController:view animated:YES completion:nil];
+}
+
 @end
