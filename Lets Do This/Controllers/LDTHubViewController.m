a8452f4a2436d9824a9b2cf7bfae55b2222c9467
Aaron Schachter <aaronschachter@gmail.com>
2015-05-13 17:13:33 -0700

    Adds user firstName to first row in Hub VC

--- Lets Do This/Controllers/LDTHubViewController.m
+++ Lets Do This/Controllers/LDTHubViewController.m
@@ -7,7 +7,34 @@
 //
 
 #import "LDTHubViewController.h"
+#import "DSOSession.h"
+#import "DSOUser.h"
+
+@interface LDTHubViewController ()
+@property (strong, nonatomic) DSOUser *user;
+@end
 
 @implementation LDTHubViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.user = [DSOSession currentSession].user;
+    NSLog(@"%@", self.user);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"profileCell" forIndexPath:indexPath];
+    cell.textLabel.text = self.user.firstName;
+    cell.userInteractionEnabled = NO;
+    return cell;
+}
 
 @end
