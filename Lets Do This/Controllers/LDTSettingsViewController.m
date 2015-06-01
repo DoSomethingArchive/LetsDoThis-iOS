dacf6fddf0f8bdb9ac663c4a1c25d32d27fb5d7d
Aaron Schachter <aaronschachter@gmail.com>
2015-06-01 13:13:34 -0700

    Check for currentUserNotificationSettings

--- Lets Do This/Controllers/LDTSettingsViewController.m
+++ Lets Do This/Controllers/LDTSettingsViewController.m
@@ -16,33 +16,46 @@
 
 @implementation LDTSettingsViewController
 
-- (void)viewDidLoad {
-    [self.notificationsSwitch setOn:NO];
-    if ([[UIApplication sharedApplication] isRegisteredForRemoteNotifications]) {
-        NSLog(@"Is registered");
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    UIUserNotificationSettings *grantedSettings = [[UIApplication sharedApplication] currentUserNotificationSettings];
+    if (grantedSettings.types == UIUserNotificationTypeNone) {
+        [self.notificationsSwitch setOn:NO];
+    }
+    else {
         [self.notificationsSwitch setOn:YES];
     }
 }
 
-- (IBAction)notificationsChanged:(id)sender {
-    if (self.notificationsSwitch.isOn) {
-        NSLog(@"User tapped ON for notifications");
-        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
-                                                        UIUserNotificationTypeBadge |
-                                                        UIUserNotificationTypeSound);
-        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
-                                                                                 categories:nil];
-        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
-        [[UIApplication sharedApplication] registerForRemoteNotifications];
-    }
-    else {
-         NSLog(@"User tapped OFF for notifications");
+- (void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 1) {
+        [self displayNotificationsInfo];
     }
 }
+
 
 - (IBAction)logoutTapped:(id)sender {
     [self confirmLogout];
 }
+
+- (void) displayNotificationsInfo {
+    UIAlertController *view = [UIAlertController alertControllerWithTitle:@"Notification settings"
+                                                                  message:@"Change your notification settings from Settings > Lets Do This."
+                                                           preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction *confirm = [UIAlertAction
+                              actionWithTitle:@"Oh, ok"
+                              style:UIAlertActionStyleDefault
+                              handler:^(UIAlertAction * action)
+                              {
+                                  [view dismissViewControllerAnimated:YES completion:nil];
+                              }];
+
+    [view addAction:confirm];
+    [self presentViewController:view animated:YES completion:nil];
+}
+
 
 - (void) confirmLogout {
     UIAlertController *view = [UIAlertController alertControllerWithTitle:@"Are you sure you want to log out?"
