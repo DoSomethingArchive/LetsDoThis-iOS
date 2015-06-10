a782fec78a11ca6fc8e79d10083923c6cc2f9186
Aaron Schachter <aaronschachter@gmail.com>
2015-06-10 09:52:56 -0700

    Adds LDTReportbackSubmitViewController with skeleton form fields

---
+++ Lets Do This/Controllers/LDTReportbackSubmitViewController.m
@@ -0,0 +1,22 @@
+//
+//  LDTReportbackSubmitViewController.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 6/10/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTReportbackSubmitViewController.h"
+
+@interface LDTReportbackSubmitViewController ()
+
+@end
+
+@implementation LDTReportbackSubmitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Prove it";
+}
+
+@end
