d61ead1177f9f915d8b8653126fbb9753b5ceec4
Aaron Schachter <aaronschachter@gmail.com>
2015-07-01 14:49:01 -0700

    Adds LDTUserSignupCodeView

---
+++ Lets Do This/Views/Login/LDTUserSignupCodeView.h
@@ -0,0 +1,14 @@
+//
+//  LDTUserSignupCodeView.h
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/1/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDTUserSignupCodeView : UIView
+@property (weak, nonatomic) IBOutlet UILabel *headerLabel;
+
+@end
---
+++ Lets Do This/Views/Login/LDTUserSignupCodeView.m
@@ -0,0 +1,27 @@
+//
+//  LDTUserSignupCodeView.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/1/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTUserSignupCodeView.h"
+@interface LDTUserSignupCodeView()
+@property (weak, nonatomic) IBOutlet UITextField *firstTextField;
+@property (weak, nonatomic) IBOutlet UITextField *secondTextField;
+@property (weak, nonatomic) IBOutlet UITextField *thirdTextField;
+
+@end
+
+@implementation LDTUserSignupCodeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
