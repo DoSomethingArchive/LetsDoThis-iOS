f27a448f49b02e6283a87beda6e64286a7bb8445
Aaron Schachter <aaronschachter@gmail.com>
2015-07-01 19:23:50 -0700

    Adds Equal Width constraints, sets SignupCodeView UITextFieldDelegates

--- Lets Do This/Views/Login/LDTUserSignupCodeView.h
+++ Lets Do This/Views/Login/LDTUserSignupCodeView.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "InterfaceBuilderView.h"
 
-@interface LDTUserSignupCodeView : InterfaceBuilderView
+@interface LDTUserSignupCodeView : InterfaceBuilderView <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *headerLabel;
 
 @end
