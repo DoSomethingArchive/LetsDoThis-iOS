5506d00c9c67d5aaaeea6f8b6e8fb0172b150a70
Aaron Schachter <aaronschachter@gmail.com>
2015-07-06 14:48:31 -0700

    Moves LDTUserRegisterFieldsView and LDTBaseUserLoginHeaderView into the UserRegisterViewController

--- Lets Do This/Controllers/LDTUserRegisterViewController.m
+++ Lets Do This/Controllers/LDTUserRegisterViewController.m
@@ -7,18 +7,26 @@
 //
 
 #import "LDTUserRegisterViewController.h"
-#import "LDTBaseUserLoginHeaderView.h"
 #import "LDTUserSignupCodeView.h"
 #import "LDTTheme.h"
 #import "LDTButton.h"
 
 @interface LDTUserRegisterViewController ()
 
-@property (weak, nonatomic) IBOutlet LDTUserRegisterFieldsView *userRegisterFieldsView;
 @property (weak, nonatomic) IBOutlet LDTButton *submitButton;
-@property (weak, nonatomic) IBOutlet LDTBaseUserLoginHeaderView *headerView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet LDTUserSignupCodeView *signupCodeView;
+
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *mobileTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *birthdayTextField;
+@property (weak, nonatomic) IBOutlet UILabel *headerPrimaryLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (strong, nonatomic) UITextField *activeField;
 
 - (IBAction)buttonTapped:(id)sender;
 
@@ -39,23 +47,68 @@
 
 -(void)viewDidLoad {
 	[super viewDidLoad];
-	self.userRegisterFieldsView.delegate = self;
-    self.submitButton.backgroundColor = [LDTTheme clickyBlue];
-    [self.submitButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+
     [self.submitButton setTitle:@"Create account" forState:UIControlStateNormal];
-    self.headerView.headerLabel.text = @"Tell us about yourself!";
-    self.headerView.imageView.image = [UIImage imageNamed:@"plus-icon"];
-    self.headerView.headerLabel.textColor = [UIColor whiteColor];
-    self.signupCodeView.headerLabel.font = [LDTTheme font];
+    self.headerPrimaryLabel.text = @"Tell us about yourself!";
     self.signupCodeView.headerLabel.text = @"If you received a code from a friend, enter it here (optional)";
-    self.signupCodeView.headerLabel.textColor = [UIColor whiteColor];
-    self.signupCodeView.headerLabel.textAlignment = NSTextAlignmentCenter;
+
+    [self registerForKeyboardNotifications];
+    [self theme];
+    [self initDatePicker];
+}
+
+- (void)theme {
+    self.imageView.image = [UIImage imageNamed:@"plus-icon"];
     UIImage *backgroundImage = [UIImage imageNamed:@"bg-lightning"];
     UIImageView *backgroundImageView=[[UIImageView alloc]initWithFrame:self.view.frame];
     backgroundImageView.image=backgroundImage;
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg-lightning"]];
-    [self registerForKeyboardNotifications];
+
+    self.submitButton.backgroundColor = [LDTTheme clickyBlue];
+    [self.submitButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+
+    UIFont *font = [LDTTheme font];
+    self.firstNameTextField.font = font;
+    [self.firstNameTextField setKeyboardType:UIKeyboardTypeNamePhonePad];
+    self.lastNameTextField.font = font;
+    [self.lastNameTextField setKeyboardType:UIKeyboardTypeNamePhonePad];
+    self.emailTextField.font = font;
+    [self.emailTextField setKeyboardType:UIKeyboardTypeEmailAddress];
+    self.mobileTextField.font = font;
+    [self.mobileTextField setKeyboardType:UIKeyboardTypeNumberPad];
+    self.passwordTextField.font = font;
+    self.birthdayTextField.font = font;
+    self.headerPrimaryLabel.font = font;
+    self.headerPrimaryLabel.textAlignment = NSTextAlignmentCenter;
+    self.headerPrimaryLabel.textColor = [UIColor whiteColor];
+    self.signupCodeView.headerLabel.font = [LDTTheme font];
+    self.signupCodeView.headerLabel.textColor = [UIColor whiteColor];
+    self.signupCodeView.headerLabel.textAlignment = NSTextAlignmentCenter;
 }
+
+- (void)initDatePicker {
+    // Create datePicker for birthdayTextField.
+    UIDatePicker *datePicker = [[UIDatePicker alloc] init];
+    datePicker.datePickerMode = UIDatePickerModeDate;
+    [datePicker addTarget:self action:@selector(updateBirthdayField:)
+         forControlEvents:UIControlEventValueChanged];
+    datePicker.maximumDate = [NSDate date];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDate *currentDate = [NSDate date];
+    NSDateComponents *comps = [[NSDateComponents alloc] init];
+    [comps setYear:-80];
+    NSDate *minDate = [calendar dateByAddingComponents:comps toDate:currentDate options:0];
+    datePicker.minimumDate = minDate;
+    [self.birthdayTextField setInputView:datePicker];
+}
+
+- (void)updateBirthdayField:(UIDatePicker *)sender
+{
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"MM/dd/YYYY"];
+    self.birthdayTextField.text = [df stringFromDate:sender.date];
+}
+
 
 - (void)registerForKeyboardNotifications
 {
@@ -69,6 +122,10 @@
     
 }
 
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    self.activeField = textField;
+}
+
 // Called when the UIKeyboardDidShowNotification is sent.
 // @see https://developer.apple.com/library/ios/documentation/StringsTextFonts/Conceptual/TextAndWebiPhoneOS/KeyboardManagement/KeyboardManagement.html#//apple_ref/doc/uid/TP40009542-CH5-SW7
 
@@ -77,15 +134,13 @@
     NSDictionary* info = [aNotification userInfo];
     CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
 
-    // @todo: Determine whether or not we are in the userRegisterFieldsView or the SignupCodeView.  Right now this assumes we're in userRegisterFieldsView.
-    UITextField *activeField = self.userRegisterFieldsView.activeField;
     UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
     self.scrollView.contentInset = contentInsets;
     self.scrollView.scrollIndicatorInsets = contentInsets;
     CGRect aRect = self.view.frame;
     aRect.size.height -= kbSize.height;
-    if (!CGRectContainsPoint(aRect, activeField.frame.origin) ) {
-        [self.scrollView scrollRectToVisible:activeField.frame animated:YES];
+    if (!CGRectContainsPoint(aRect, self.activeField.frame.origin) ) {
+        [self.scrollView scrollRectToVisible:self.activeField.frame animated:YES];
     }
 }
 
@@ -100,6 +155,17 @@
 - (IBAction)buttonTapped:(id)sender {
 //    NSLog(@"firstName %@", [self.userRegisterFieldsView getValues]);
 //    [self.userRegisterFieldsView processSuccessful:YES];
+}
+
+- (NSDictionary *)getValues {
+    return @{
+             @"first_name": self.firstNameTextField.text,
+             @"last_name": self.lastNameTextField.text,
+             @"email": self.emailTextField.text,
+             @"mobile": self.mobileTextField.text,
+             @"password": self.passwordTextField.text,
+             @"birthdate": self.birthdayTextField.text
+             };
 }
 
 #pragma mark - LDTUserRegisterFieldsViewDelegate
