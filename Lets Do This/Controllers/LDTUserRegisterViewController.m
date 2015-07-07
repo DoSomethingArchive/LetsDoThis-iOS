1cecd64da16e3c6e078ae32b06ce3c0b536bbf74
Aaron Schachter <aaronschachter@gmail.com>
2015-07-06 21:59:43 -0700

    Removes unnecessary initWithNibName

--- Lets Do This/Controllers/LDTUserRegisterViewController.m
+++ Lets Do This/Controllers/LDTUserRegisterViewController.m
@@ -34,16 +34,16 @@
 
 @implementation LDTUserRegisterViewController
 
--(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-	
-	if (self) {
-
-	}
-	
-	return self;
-	
-}
+//-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+//	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//	
+//	if (self) {
+//
+//	}
+//	
+//	return self;
+//	
+//}
 
 -(void)viewDidLoad {
 	[super viewDidLoad];
