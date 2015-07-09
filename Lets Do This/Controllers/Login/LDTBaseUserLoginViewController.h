a5d150465558fdd7dee8a47cf3b5cfa04fed7639
Aaron Schachter <aaronschachter@gmail.com>
2015-07-09 09:25:15 -0700

    Adds LDTBaseUserLoginViewController

---
+++ Lets Do This/Controllers/Login/LDTBaseUserLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LDTBaseUserLoginViewController.h
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/9/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDTBaseUserLoginViewController : UIViewController
+
+@end
