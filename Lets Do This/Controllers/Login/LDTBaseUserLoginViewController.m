df37053832490cde18d57b05400f6d4c946433a6
Aaron Schachter <aaronschachter@gmail.com>
2015-07-09 09:25:15 -0700

    Adds LDTBaseUserLoginViewController

---
+++ Lets Do This/Controllers/Login/LDTBaseUserLoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LDTBaseUserLoginViewController.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/9/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTBaseUserLoginViewController.h"
+
+@interface LDTBaseUserLoginViewController ()
+
+@end
+
+@implementation LDTBaseUserLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
