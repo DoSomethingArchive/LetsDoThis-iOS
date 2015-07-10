05db7544f506491fab6686ca2787b00241eafb78
Aaron Schachter <aaronschachter@gmail.com>
2015-07-09 19:37:05 -0700

    Loads UserProfile if DSOSession hasCachedSession

---
+++ Lets Do This/Controllers/Base/LDTLoadingViewController.m
@@ -0,0 +1,37 @@
+//
+//  LDTLoadingViewController.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/9/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTLoadingViewController.h"
+
+@interface LDTLoadingViewController ()
+
+@end
+
+@implementation LDTLoadingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
