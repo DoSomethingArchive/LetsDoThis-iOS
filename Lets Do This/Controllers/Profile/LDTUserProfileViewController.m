77f9a24edbad623136dd22b9404bcfe53fe47240
Aaron Schachter <aaronschachter@gmail.com>
2015-07-09 17:34:31 -0700

    Adds empty UserProfileView and UserProfileViewController

---
+++ Lets Do This/Controllers/Profile/LDTUserProfileViewController.m
@@ -0,0 +1,22 @@
+//
+//  LDTUserProfileViewController.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/9/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTUserProfileViewController.h"
+
+@interface LDTUserProfileViewController ()
+
+@end
+
+@implementation LDTUserProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
