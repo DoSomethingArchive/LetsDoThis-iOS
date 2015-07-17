c6b27aa56e7274a6d00018e141dd2d83b9c019dc
Aaron Schachter <aaronschachter@gmail.com>
2015-07-16 17:48:44 -0700

    Adds tableView and displays all Campaigns

--- Lets Do This/Controllers/Campaign/LDTCampaignListViewController.m
+++ Lets Do This/Controllers/Campaign/LDTCampaignListViewController.m
@@ -10,15 +10,29 @@
 #import "DSOSession.h"
 #import "DSOCampaign.h"
 
-@interface LDTCampaignListViewController ()
+@interface LDTCampaignListViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSMutableArray *campaigns;
 @end
 
+// Stores name for a resuable TableView cell. per  http://www.guilmo.com/how-to-create-a-simple-uitableview-with-static-data/
+static NSString *cellIdentifier;
+
 @implementation LDTCampaignListViewController
+
+#pragma UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"Let's Do This";
     self.campaigns = [[NSMutableArray alloc] init];
+
+    cellIdentifier = @"rowCell";
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
     DSOSession *session = [DSOSession currentSession];
     [session.api fetchCampaignsWithCompletionHandler:^(NSDictionary *response) {
 
@@ -28,10 +42,23 @@
             [self.campaigns addObject:campaign];
 
         }
+        [self.tableView reloadData];
 
     } errorHandler:^(NSError *error) {
         NSLog(@"error %@", error);
     }];
 }
 
+#pragma UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.campaigns count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    DSOCampaign *campaign = (DSOCampaign *)self.campaigns[indexPath.row];
+    cell.textLabel.text = campaign.title;
+    return cell;
+}
 @end
