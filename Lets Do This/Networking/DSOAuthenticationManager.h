64f313c08c4160184d435ad5e5d434c48e6ce8b8
Aaron Schachter <aaronschachter@gmail.com>
2015-07-23 14:07:40 -0700

    Starts on DSOAuthenticationManager class

---
+++ Lets Do This/Networking/DSOAuthenticationManager.h
@@ -0,0 +1,30 @@
+//
+//  DSOAuthenticationManager.h
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 7/23/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DSOUser.h"
+
+@interface DSOAuthenticationManager : NSObject
+
+@property (strong, nonatomic) DSOUser *user;
+
++ (DSOAuthenticationManager *)sharedInstance;
+
+- (void)loginWithEmail:(NSString *)email
+              password:(NSString *)password
+     completionHandler:(void(^)(NSDictionary *))completionHandler
+          errorHandler:(void(^)(NSError *))errorHandler;
+
+- (BOOL)hasCachedSession;
+
+- (void)connectWithCachedSessionWithCompletionHandler:(void(^)(NSDictionary *))completionHandler
+                                         errorHandler:(void(^)(NSError *))errorHandler;
+
+- (void)logoutWithCompletionHandler:(void(^)(NSDictionary *))completionHandler
+                       errorHandler:(void(^)(NSError *))errorHandler;
+@end
