6da7c15c065a8814e3cfe8345ea726ed546ae786
Aaron Schachter <aaronschachter@gmail.com>
2015-08-04 09:28:51 -0700

    Correct spelling, fixes #175

--- Lets Do This/Networking/DSOAPI.m
+++ Lets Do This/Networking/DSOAPI.m
@@ -33,7 +33,7 @@
 
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        // @todo: Don't do it this way.
+        // @todo: When we're ready to test against prod, setup conditional if DEBUG.
         _sharedInstance = [[self alloc] initWithApiKey:keysDict[@"northstarTestKey"]];
     });
 
@@ -66,11 +66,8 @@
     return self;
 }
 
-#warning You should just be returning _phoenixBaseURL here or you'll get a recursive loop
-// since a call to `self.phoenixBaseURL` accesses your method - (NSString *)pheonixBaseUrl
-// The only reason this isn't happening is because you've misspelled 'phoenix' in your method
-- (NSString *)pheonixBaseUrl {
-    return self.phoenixBaseURL;
+- (NSString *)phoenixBaseUrl {
+    return _phoenixBaseURL;
 }
 
 #pragma DSOAPI
