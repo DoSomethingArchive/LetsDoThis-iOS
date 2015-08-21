3ad46ebefe6159331599d942b015145fd66947a3
Aaron Schachter <aaronschachter@gmail.com>
2015-08-21 12:13:54 -0700

    Adds properties to DSOReportbackItem

--- Lets Do This/Models/DSOReportbackItem.m
+++ Lets Do This/Models/DSOReportbackItem.m
@@ -11,7 +11,11 @@
 
 @interface DSOReportbackItem()
 
+@property (nonatomic, strong, readwrite) DSOCampaign *campaign;
+@property (nonatomic, strong, readwrite) DSOUser *user;
+@property (nonatomic, assign, readwrite) NSInteger quantity;
 @property (nonatomic, assign, readwrite) NSInteger reportbackItemID;
+@property (nonatomic, strong, readwrite) NSString *caption;
 @property (nonatomic, strong, readwrite) NSURL *imageURL;
 
 @end
@@ -20,11 +24,18 @@
 
 - (id)initWithDict:(NSDictionary*)dict {
     self = [super init];
-    if(self) {
-        self.reportbackItemID = [dict valueForKeyAsInt:@"id" nullValue:self.reportbackItemID];
+
+    if (self) {
+        self.reportbackItemID = [dict valueForKeyAsInt:@"id" nullValue:0];
+        self.quantity = [[dict valueForKeyPath:@"reportback"] valueForKeyAsInt:@"quantity" nullValue:0];
+        self.caption = [dict valueForKeyAsString:@"caption"];
         NSString *imagePath = [[dict valueForKeyPath:@"media"] valueForKeyAsString:@"uri" nullValue:nil];
         self.imageURL = [NSURL URLWithString:imagePath];
+        NSInteger campaignID = [[dict valueForKeyPath:@"campaign.id"] intValue];
+        // @todo: If an active DSOCampaign doesn't exist, use the dictionary to create a DSOCampaign to expose the Campaign Title 
+        self.campaign = [[DSOUserManager sharedInstance] activeMobileAppCampaignWithId:campaignID];
     }
+
     return self;
 }
 
