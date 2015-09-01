8029ace93378bf649e943b11272857375ac7c1f0
Tong Xiang <tong.xiang2@gmail.com>
2015-09-01 15:00:33 -0400

    add empty view controller and xib files, new directories

---
+++ Lets Do This/Controllers/Onboarding/LDTActionsOnboardingViewController.h
@@ -0,0 +1,13 @@
+//
+//  LDTActionsOnboardingViewController.h
+//  Lets Do This
+//
+//  Created by Tong Xiang on 9/1/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDTActionsOnboardingViewController : UIViewController
+
+@end
---
+++ Lets Do This/Controllers/Onboarding/LDTInvitesOnboardingViewController.m
@@ -0,0 +1,37 @@
+//
+//  LDTInvitesOnboardingViewController.m
+//  Lets Do This
+//
+//  Created by Tong Xiang on 9/1/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTInvitesOnboardingViewController.h"
+
+@interface LDTInvitesOnboardingViewController ()
+
+@end
+
+@implementation LDTInvitesOnboardingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
