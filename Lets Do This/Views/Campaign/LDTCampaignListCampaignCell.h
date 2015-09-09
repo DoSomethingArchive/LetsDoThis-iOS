f450c1377e251d1f22eff426f00f46553533b7ef
Aaron Schachter <aaronschachter@gmail.com>
2015-09-09 08:49:44 -0700

    Adds signupIndicatorView outlet, set to green if signed up

--- Lets Do This/Views/Campaign/LDTCampaignListCampaignCell.h
+++ Lets Do This/Views/Campaign/LDTCampaignListCampaignCell.h
@@ -22,6 +22,7 @@
 @property (strong, nonatomic) NSString *taglineLabelText;
 @property (strong, nonatomic) NSURL *imageViewImageURL;
 @property (nonatomic, assign, getter=isExpanded) BOOL expanded;
+@property (nonatomic, assign) BOOL isSignedUp;
 
 #warning Descriptive naming in xib files
 // The interface objects in the xib files (labels, views, etc.) should be named descriptively
