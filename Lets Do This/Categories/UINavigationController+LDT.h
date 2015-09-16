a786b19d5af9718d59e8647acb0810e1fd8eb70d
Aaron Schachter <aaronschachter@gmail.com>
2015-09-16 14:19:57 -0700

    Adds UINavigationController+LDT category

---
+++ Lets Do This/Categories/UINavigationController+LDT.h
@@ -0,0 +1,20 @@
+//
+//  UINavigationController+LDT.h
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 9/16/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, LDTNavigationBarStyle) {
+    LDTNavigationBarStyleNormal,
+    LDTNavigationBarStyleClear
+};
+
+@interface UINavigationController (LDT)
+
+- (void)styleNavigationBar:(LDTNavigationBarStyle)style;
+
+@end
--- Lets Do This/Views/Base/LDTTheme.h
+++ Lets Do This/Views/Base/LDTTheme.h
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "UIImageView+LDT.h"
+#import "UINavigationController+LDT.h"
 #import "LDTNavigationController.h"
 #import "LDTButton.h"
 #import "LDTMessage.h"
