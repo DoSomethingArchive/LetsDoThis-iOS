f02f24556aea706eb041be5be26081e41da2f580
Aaron Schachter <aaronschachter@gmail.com>
2015-09-16 16:02:11 -0700

    Removes code review comment - was fixed in #351

--- Lets Do This/Views/Campaign/LDTCampaignListCampaignCell.h
+++ Lets Do This/Views/Campaign/LDTCampaignListCampaignCell.h
@@ -25,11 +25,6 @@
 @property (nonatomic, assign, getter=isExpanded) BOOL expanded;
 @property (nonatomic, assign) BOOL signedUp;
 
-#warning Descriptive naming in xib files
-// The interface objects in the xib files (labels, views, etc.) should be named descriptively
-// Not just "Label," "ImageView," etc. You don't have to use camelcase when naming them; you can use "Title Label" or "Action View"
-// That way, if someone else new (or even you at some point down the road) sees the files, they instantly know what everything's for
-
 @end
 
 @protocol LDTCampaignListCampaignCellDelegate <NSObject>
