d343e0ed6c784edd5279ba0519a1fef5bbbffa6e
Aaron Schachter <aaronschachter@gmail.com>
2015-09-17 12:05:56 -0700

    Adds skeleton LDTUpdateAvatarView ViewController, creates Settings groups

---
+++ Lets Do This/Controllers/Settings/LDTUpdateAvatarViewController.m
@@ -0,0 +1,23 @@
+//
+//  LDTUpdateAvatarViewController.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 9/17/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTUpdateAvatarViewController.h"
+
+@interface LDTUpdateAvatarViewController ()
+
+@end
+
+@implementation LDTUpdateAvatarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
