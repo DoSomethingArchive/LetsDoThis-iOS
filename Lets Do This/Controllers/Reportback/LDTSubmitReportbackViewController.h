4fe8d6af912d355c28bbc46f28eb57b4160695a5
Aaron Schachter <aaronschachter@gmail.com>
2015-09-16 18:36:15 -0700

    Adds skeleton LDTSubmitReportbackView and LDTSubmitReportbackViewController files

---
+++ Lets Do This/Controllers/Reportback/LDTSubmitReportbackViewController.h
@@ -0,0 +1,13 @@
+//
+//  LDTSubmitReportbackViewController.h
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 9/16/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTBaseViewController.h"
+
+@interface LDTSubmitReportbackViewController : LDTBaseViewController
+
+@end
---
+++ Lets Do This/Controllers/Reportback/LDTSubmitReportbackViewController.m
@@ -0,0 +1,13 @@
+//
+//  LDTSubmitReportbackViewController.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 9/16/15.
+//  Copyright (c) 2015 Do Something. All rights reserved.
+//
+
+#import "LDTSubmitReportbackViewController.h"
+
+@implementation LDTSubmitReportbackViewController
+
+@end
