f92ce43e690107bea6d52e532891cd2fd23fbd96
Aaron Schachter <aaronschachter@gmail.com>
2015-09-22 15:47:23 -0700

    Combines enable and disable into one enable: method

--- Lets Do This/Views/Base/LDTButton.m
+++ Lets Do This/Views/Base/LDTButton.m
@@ -17,9 +17,6 @@
     if (self) {
         [[self titleLabel] setFont:[LDTTheme fontBold]];
         self.layer.cornerRadius = 4;
-		
-		// Can we not set these here once? Don't think we should have to set each time on disabled or not. Also,
-		// Don't we want UIControlStateDisabled for enabled = NO?
 		[self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
 		[self setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
     }
@@ -27,7 +24,6 @@
     return self;
 }
 
-// These both can be combined into one method
 -(void)enable:(BOOL)enabled {
 	if (enabled) {
 		self.backgroundColor = [LDTTheme ctaBlueColor];
@@ -37,18 +33,6 @@
 	}
 	
 	self.enabled = enabled;
-}
-
--(void)disable {
-    self.enabled = NO;
-    self.backgroundColor = [LDTTheme disabledGrayColor];
-    [self setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
-}
-
--(void)enable {
-    self.enabled = YES;
-    self.backgroundColor = [LDTTheme ctaBlueColor];
-    [self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
 }
 
 @end
