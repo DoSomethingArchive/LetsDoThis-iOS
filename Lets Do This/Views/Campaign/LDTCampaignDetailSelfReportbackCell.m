7601a9e79f4f37b4d07d3609424d2c7771e29c8d
Aaron Schachter <aaronschachter@gmail.com>
2015-09-24 15:09:54 -0700

    Starts on LDTCampaignDetailSelfReportbackCell

---
+++ Lets Do This/Views/Campaign/LDTCampaignDetailSelfReportbackCell.m
@@ -0,0 +1,29 @@
+//
+//  LDTCampaignDetailUserReportbackCell.m
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 9/24/15.
+//  Copyright © 2015 Do Something. All rights reserved.
+//
+
+#import "LDTCampaignDetailSelfReportbackCell.h"
+#import "LDTReportbackItemDetailView.h"
+#import "LDTTheme.h"
+
+@interface LDTCampaignDetailSelfReportbackCell()
+
+@property (weak, nonatomic) IBOutlet LDTReportbackItemDetailView *detailView;
+@property (weak, nonatomic) IBOutlet LDTButton *sharePhotoButton;
+
+@end
+
+@implementation LDTCampaignDetailSelfReportbackCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    [self.sharePhotoButton setTitle:@"Share your photo".uppercaseString forState:UIControlStateNormal];
+    [self.sharePhotoButton enable:YES];
+}
+
+@end
