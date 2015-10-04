7634900416f338e8d2660fa4978ac912b0597296
Evan Roth <evanroth@me.com>
2015-10-03 21:54:44 -0400

    More fixes.

--- Lets Do This/Views/Campaign/CampaignCollectionViewCellContainer.h
+++ Lets Do This/Views/Campaign/CampaignCollectionViewCellContainer.h
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-static NSString *CollectionViewCellIdentifier = @"CollectionViewCellIdentifier";
-
 @interface CampaignCollectionViewCellContainer : UICollectionViewCell
 
 @property (nonatomic, strong) UICollectionView *innerCollectionView;
