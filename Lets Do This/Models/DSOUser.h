98206c0b4692bb00a78ff090c43c806692b5781a
Tong Xiang <tong.xiang2@gmail.com>
2015-10-06 13:21:17 -0400

    move remove photo logic into DSOUserManager

--- Lets Do This/Models/DSOUser.h
+++ Lets Do This/Models/DSOUser.h
@@ -13,20 +13,20 @@
 
 @interface DSOUser : NSObject
 
-@property (nonatomic, strong, readonly) NSString *userID;
-@property (nonatomic, strong, readonly) NSString *sessionToken;
 @property (nonatomic, strong, readonly) NSString *countryCode;
 @property (nonatomic, strong, readonly) NSString *countryName;
 @property (nonatomic, strong, readonly) NSString *displayName;
-@property (nonatomic, strong, readonly) NSString *firstName;
 @property (nonatomic, strong, readonly) NSString *email;
+@property (nonatomic, strong, readonly) NSString *firstName;
 @property (nonatomic, strong, readonly) NSString *mobile;
+@property (nonatomic, strong, readonly) NSString *photoNameString;
+@property (nonatomic, strong, readonly) NSString *sessionToken;
+@property (nonatomic, strong, readonly) NSString *userID;
 @property (nonatomic, strong, readonly) UIImage *photo;
 @property (nonatomic, strong) NSMutableArray *campaignSignups;
 
 - (instancetype)initWithDict:(NSDictionary *)dict;
 - (void)setPhoto:(UIImage *)image;
-- (void)removePhoto;
 // Checks if the User is the logged-in User.
 - (BOOL)isLoggedInUser;
 - (BOOL)isDoingCampaign:(DSOCampaign *)campaign;
