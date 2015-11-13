99130abf105094453fe37ab433f5e879a74462b6
Aaron Schachter <aaronschachter@gmail.com>
2015-11-13 10:19:01 -0800

    Hotfix (or permanent?) for #504

--- Lets Do This/Categories/UIImageView+LDT.m
+++ Lets Do This/Categories/UIImageView+LDT.m
@@ -21,7 +21,8 @@
     CALayer *sublayer = [CALayer layer];
     sublayer.backgroundColor = [UIColor blackColor].CGColor;
     sublayer.opacity = 0.3;
-    sublayer.frame = self.frame;
+    // Weird rendering/timing bug (#504) where our imageView's frame.size.width is set to the xib file's width here, so we pass the mainScreen width to stretch further than xib width.
+    sublayer.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, [UIScreen mainScreen].bounds.size.width, self.frame.size.height);
     [self.layer addSublayer:sublayer];
 }
 
