a2232245d64ff6f80dfa5b8e9b8a7aa93020457d
Aaron Schachter <aaronschachter@gmail.com>
2015-11-25 13:05:51 -0800

    Adds LDTProfileReportbackItemTableViewCell

---
+++ Lets Do This/Views/Profile/LDTProfileReportbackItemTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  LDTProfileReportbackItemTableViewCell.h
+//  Lets Do This
+//
+//  Created by Aaron Schachter on 11/25/15.
+//  Copyright © 2015 Do Something. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LDTReportbackItemDetailView.h"
+
+@interface LDTProfileReportbackItemTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet LDTReportbackItemDetailView *detailView;
+
+@end
