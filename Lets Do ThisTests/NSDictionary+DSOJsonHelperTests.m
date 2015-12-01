83981ebc2e5cafa598b76e8a55f21a2da9db5f5f
Tong Xiang <tong.xiang2@gmail.com>
2015-12-01 16:24:46 -0500

    add tests for NSDictionary+DSOJsonHelper.m

--- Lets Do ThisTests/NSDictionary+DSOJsonHelperTests.m
+++ Lets Do ThisTests/NSDictionary+DSOJsonHelperTests.m
@@ -5,10 +5,20 @@
 //  Created by Tong Xiang on 11/25/15.
 //  Copyright © 2015 Do Something. All rights reserved.
 //
+/*
+    The NSDictionary+DSOJsonHelper category extends NSDictionary to include methods    
+    which parse the string-based NSDictionary that a JSON object is ingested as. 
+    For instance, valueForKeyAsInt converts a integer value, stored as a string, 
+    into an integer. We test those functions here.
+*/
 
 #import <XCTest/XCTest.h>
+#import "NSDictionary+DSOJsonHelper.h"
+#import "NSDate+DSO.h"
 
 @interface NSDictionary_DSOJsonHelperTests : XCTestCase
+
+@property (nonatomic) NSDictionary *campaignJSONData;
 
 @end
 
@@ -16,17 +26,8 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-    
-    // Create JSON data object
-    
-    // http://stackoverflow.com/questions/11932864/how-to-create-json-in-objective-c
-    
-    NSDictionary *setUser = [NSDictionary dictionaryWithObjectsAndKeys:[@"u" stringByAppendingString:@"100001"], @"id", @"GET_USER_INFO", @"command", @"", @"value", nil];
-    NSError *error;
-    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:setUser options:NSJSONWritingPrettyPrinted error:&error];
-    
-    NSLog(@"%@", jsonData);
+
+    self.campaignJSONData = [NSDictionary dictionaryWithObjectsAndKeys:@"5052", @"id", @"#BookItForward", @"title", @"Create a \'take one, leave one\' book swap to promote literacy.", @"tagline", @"1422290632", @"created_at", @"true", @"staff_pick", @"111111111111111", @"doubleNumber", @"2011-02-01T10:57:55-08:00", @"dateCreated", nil];
 }
 
 - (void)tearDown {
@@ -35,43 +36,53 @@
 }
 
 - (void)testValueForJSONKey {
-    // valueForJSONKey
-    
-    // This is only used within the `testValueForKeyAsString` <--
-    
-    // I need to instantiate an NSDictionary with some values
-    XCTAssert(YES, @"Pass");
-    
+    XCTAssertEqualObjects([self.campaignJSONData valueForJSONKey:@"id"], @"5052");
+}
+
+- (void)testFalseValueForJSONKey {
+    XCTAssertNotEqual([self.campaignJSONData valueForJSONKey:@"id"], NULL);
 }
 
 - (void)testValueForKeyAsString {
-    // Parses JSON to return the value of a key, in string format.
-    // valueForKeyAsString
-    XCTAssert(YES, @"Pass");
+    XCTAssertEqualObjects([self.campaignJSONData valueForKeyAsString:@"title"], @"#BookItForward");
+}
+
+- (void)testFalseValueForKeyAsString {
+    XCTAssertNotEqualObjects([self.campaignJSONData valueForKeyAsString:@"title"], @"Super Stress Face");
 }
 
 - (void)testValueForKeyAsInt {
-    // Parses JSON to return the value of a key, in int format.
-    // valueForKeyAsInt
-    XCTAssert(YES, @"Pass");
+    XCTAssertEqual([self.campaignJSONData valueForKeyAsInt:@"id" nullValue:0], 5052);
+}
+
+- (void)testFalseValueForKeyAsInt {
+    XCTAssertNotEqual([self.campaignJSONData valueForKeyAsInt:@"id" nullValue:0], 101);
 }
 
 - (void)testValueForKeyAsDouble {
-    // valueForKeyAsDouble
-    XCTAssert(YES, @"Pass");
+    double doubleNumber = [[NSNumber numberWithDouble:111111111111111] doubleValue];
+    XCTAssertEqual([self.campaignJSONData valueForKeyAsDouble:@"doubleNumber" nullValue:0], doubleNumber);
+}
+
+- (void)testFalseValueForKeyAsDouble {
+    double doubleNumber = [[NSNumber numberWithDouble:2222222222222222] doubleValue];
+    XCTAssertNotEqual([self.campaignJSONData valueForKeyAsDouble:@"doubleNumber" nullValue:0], doubleNumber);
 }
 
 - (void)testValueForKeyAsBool {
-    // valueForKeyAsBool
-    XCTAssert(YES, @"Pass");
+    XCTAssertEqual([self.campaignJSONData valueForKeyAsBool:@"staff_pick" nullValue:false], true);
+}
+
+- (void)testFalseValueForKeyAsBool {
+    XCTAssertNotEqual([self.campaignJSONData valueForKeyAsBool:@"staff_pick" nullValue:false], false);
 }
 
 - (void)testValueForKeyAsDate {
-    // valueForKeyAsDate
-    XCTAssert(YES, @"Pass");
+    XCTAssertEqual([self.campaignJSONData valueForKeyAsDate:@"dateCreated" nullValue:[NSDate dateFromISO8601String:@"2015-12-01T12:00:00-8:00"]], [NSDate dateFromISO8601String:@"2011-02-01T10:57:55-08:00"]);
 }
 
-// Test if you input invalid values--or Nil values.
+- (void)testFalseValueForKeyAsDate {
+    XCTAssertNotEqual([self.campaignJSONData valueForKeyAsDate:@"dateCreated" nullValue:[NSDate dateFromISO8601String:@"2015-12-01T12:00:00-8:00"]], [NSDate dateFromISO8601String:@"2012-02-01T10:57:55-08:00"]);
+}
 
-// LIke it's not expecting the right thing, or
 @end
