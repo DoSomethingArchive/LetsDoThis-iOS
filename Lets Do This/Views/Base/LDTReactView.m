326ee2e9dd9b96e182e2ecd954a08e5a25cec72a
Aaron Schachter <aaronschachter@gmail.com>
2016-01-12 14:08:12 -0800

    Adds tutorial code, rename as NewsStoryBox

--- Lets Do This/Views/Base/LDTReactView.m
+++ Lets Do This/Views/Base/LDTReactView.m
@@ -16,7 +16,7 @@
     NSString *urlString = @"http://localhost:8081/index.ios.bundle";
     // @todo: Pass moduleName as parameter to custom init?
     NSURL *jsCodeLocation = [NSURL URLWithString:urlString];
-    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation moduleName: @"SimpleApp" initialProperties:nil launchOptions:nil];
+    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation moduleName: @"NewsStoryBox" initialProperties:nil launchOptions:nil];
     [self addSubview:rootView];
     rootView.frame = self.bounds;
 }
--- ReactComponent/index.ios.js
+++ ReactComponent/index.ios.js
@@ -1,29 +1,107 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
 'use strict';
-
-var React = require('react-native');
-var {
+import React, {
+  AppRegistry,
+  ListView,
+  Component,
+  StyleSheet,
   Text,
+  Image,
   View
-} = React;
+} from 'react-native';
 
-var styles = React.StyleSheet.create({
- container: {
-    flex: 1,
-    justifyContent: 'center',
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var NewsStoryBox = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
   },
-  title: {
-    fontSize: 28,
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  },
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
   },
 });
 
-class SimpleApp extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome to the news!</Text>
-      </View>
-    )
-  }
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+  },
+  year: {
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
 
-React.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
+AppRegistry.registerComponent('NewsStoryBox', () => NewsStoryBox);
