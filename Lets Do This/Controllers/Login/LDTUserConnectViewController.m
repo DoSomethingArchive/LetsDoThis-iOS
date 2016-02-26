9843b2160febfe06627b6e557c28e900f8420d53
Aaron Schachter <aaronschachter@gmail.com>
2016-02-26 11:40:12 -0800

    Updates userConnect copy, removes fixed height, design cleanup

--- Lets Do This/Controllers/Login/LDTUserConnectViewController.m
+++ Lets Do This/Controllers/Login/LDTUserConnectViewController.m
@@ -38,7 +38,7 @@
     self.navigationItem.hidesBackButton = YES;
     [self styleBackBarButton];
     
-    self.headerLabel.text = @"Let’s make this official. Register an account \nto find un-boring stuff to do with your friends.";
+    self.headerLabel.text = @"Let’s make this official.\nCreate an account to find news around your top causes and ways to take action.";
     [self.registerButton setTitle:@"Register".uppercaseString forState:UIControlStateNormal];
     [self.loginButton setTitle:@"Sign in".uppercaseString forState:UIControlStateNormal];
 
