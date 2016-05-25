6046ab2d37b51f1b4f259f99bd906bdeb0527990
Aaron Schachter <aaronschachter@gmail.com>
2016-05-25 12:26:04 -0700

    Adds button to download and open file with UIDocumentInteractionController

--- Lets Do This/Controllers/Base/LDTWebViewController.m
+++ Lets Do This/Controllers/Base/LDTWebViewController.m
@@ -9,7 +9,7 @@
 #import "LDTWebViewController.h"
 #import "GAI+LDT.h"
 
-@interface LDTWebViewController () <UIWebViewDelegate>
+@interface LDTWebViewController () <UIWebViewDelegate, UIDocumentInteractionControllerDelegate>
 
 @property (strong, nonatomic) NSString *navigationTitle;
 @property (strong, nonatomic) NSString *screenName;
@@ -22,6 +22,7 @@
 
 #pragma mark - NSObject
 
+// @todo Pass optional downloadParam to conditionally display toolbar/download option.
 - (instancetype)initWithWebViewURL:(NSURL *)webViewURL title:(NSString *)navigationTitle screenName:(NSString *)screenName{
     self = [super init];
     
@@ -45,6 +46,11 @@
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:self.webViewURL];
     [webView loadRequest:urlRequest];
     [self.view addSubview:webView];
+
+    self.navigationController.toolbarHidden = NO;
+    UIBarButtonItem *downloadBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(downloadButtonTapped:)];
+    NSArray *items = [NSArray arrayWithObjects:downloadBarButtonItem, nil];
+    self.toolbarItems = items;
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -53,5 +59,32 @@
     [[GAI sharedInstance] trackScreenView:self.screenName];
 }
 
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    self.navigationController.toolbarHidden = YES;
+}
+
+#pragma mark - IBActions
+
+- (IBAction)downloadButtonTapped:(id)sender {
+    UIBarButtonItem *item = (UIBarButtonItem *)self.toolbarItems[0];
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.webViewURL];
+    NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *filePath = [documentDir stringByAppendingPathComponent:self.webViewURL.lastPathComponent];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        if (error) {
+            // @todo LDTMessage
+            NSLog(@"Download Error: %@", error.description);
+        }
+        if (data) {
+            [data writeToFile:filePath atomically:YES];
+            NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+            UIDocumentInteractionController *documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:fileURL];
+            documentInteractionController.delegate = self;
+            [documentInteractionController presentOpenInMenuFromBarButtonItem:item animated:YES];
+        }
+    }];
+}
 
 @end
